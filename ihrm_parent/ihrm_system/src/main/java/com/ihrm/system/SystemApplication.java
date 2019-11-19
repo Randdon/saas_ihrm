595e42976b7275f79cfa9a105ac3f372babe8afb
yuandong <yuand@drplant.com.cn>
2019-11-19 15:17:56 +0800

    搭建 系统 微服务

---
+++ ihrm_parent/ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
@@ -0,0 +1,23 @@
+package com.ihrm.system;
+
+import com.zhouyuan.saas.ihrm.utils.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+
+//Springboot的包扫描
+@SpringBootApplication(scanBasePackages = "com.ihrm.system")
+//jpa注解的扫描
+@EntityScan(value = "com.ihrm.domain.system")
+public class SystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker();
+    }
+
+}
---
+++ ihrm_parent/ihrm_system/src/test/java/com/zhouyuan/saas/ihrm/ihrm_system/IhrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouyuan.saas.ihrm.ihrm_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhrmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

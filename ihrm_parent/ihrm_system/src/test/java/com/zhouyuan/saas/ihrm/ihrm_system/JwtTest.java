441fefed39146909ccbae29b0cd5f41ef387c0a7
yuandong <yuand@drplant.com.cn>
2019-12-16 16:33:16 +0800

    自定义claim设置数据

--- ihrm_parent/ihrm_system/src/test/java/com/zhouyuan/saas/ihrm/ihrm_system/JwtTest.java
+++ ihrm_parent/ihrm_system/src/test/java/com/zhouyuan/saas/ihrm/ihrm_system/JwtTest.java
@@ -24,7 +24,10 @@ public class JwtTest extends IhrmSystemApplicationTests {
                 .setId("1893")//设置token里的一些内容
                 .setSubject("ChairmanMao")//设置token里的一些内容
                 .setIssuedAt(new Date())//设置token的签发时间
-                .signWith(SignatureAlgorithm.HS256, "ydss1236");//加密算法和私钥(私钥至少需要4位)
+                .signWith(SignatureAlgorithm.HS256, "ihrm")
+                .claim("companyId","18710892634")//自定义claim设置数据
+                .claim("companyName","Utopian");//自定义claim设置数据
+
         String token = jwtBuilder.compact();
         System.out.println(token);
     }
@@ -34,14 +37,18 @@ public class JwtTest extends IhrmSystemApplicationTests {
      */
     @Test
     public void parseJwt(){
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxODkzIiwic3ViIjoiQ2hhaXJtYW5NYW8iLCJpYXQiOjE1NzY0ODM2NTB9.HOcev12gcMNhJoZLrGJHJJKaKvPGvK_5hlc4Ll6uXP8";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxODkzIiwic3ViIjoiQ2hhaXJtYW5NYW8iLCJpYXQiOjE1NzY0ODQ5OTMsImNvbXBhbnlJZCI6IjE4NzEwODkyNjM0IiwiY29tcGFueU5hbWUiOiJVdG9waWFuIn0.ezGGWhV4Q_Uh6MbctZjbQrghPSEZ1Zvm2zJfsRHnztU";
         Claims claims = Jwts.parser()
-                .setSigningKey("ydss1236acbd")//私钥
+                .setSigningKey("ihrm12")//私钥
                 //TODO 此处解析时所用的密钥，如果为原密钥后面+3位的都可以解析成功，比如ihrm123，ihrmab，ihrmc都可以解析出数据
                 .parseClaimsJws(token)
                 .getBody();
         System.out.println(claims.getId());
         System.out.println(claims.getSubject());
         System.out.println(claims.getIssuedAt());
+
+        //解析获取自定义claim中的内容
+        System.out.println(claims.get("companyId"));
+        System.out.println(claims.get("companyName"));
     }
 }

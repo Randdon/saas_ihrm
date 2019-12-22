beb77eeb0fd4f7c2d5883d150504e60739af982e
YuanDong <yuand@drplant.com.cn>
2019-12-22 16:31:03 +0800

    springboot整合shiro的基本案例代码

---
+++ ihrm_parent/shiro_springboot/src/main/java/com/zhouyuan/shiro/service/UserService.java
@@ -0,0 +1,23 @@
+package com.zhouyuan.shiro.service;
+
+import com.zhouyuan.shiro.dao.UserDao;
+import com.zhouyuan.shiro.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public User findByName(String name) {
+        return this.userDao.findByUsername(name);
+    }
+
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+}

f175f2e379519fc036772bb4087f8d72b4dd1a4c
yuandong <yuand@drplant.com.cn>
2019-12-26 15:56:22 +0800

    shiro-redis配置类；认证或鉴权失败公共controller；redis地址、端口配置

---
+++ ihrm_parent/ihrm_common/src/main/java/com/zhouyuan/saas/ihrm/controller/AuthErrorController.java
@@ -0,0 +1,18 @@
+package com.zhouyuan.saas.ihrm.controller;
+
+import com.zhouyuan.saas.ihrm.entity.Result;
+import com.zhouyuan.saas.ihrm.entity.ResultCode;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @description: shiro认证或鉴权失败controller
+ * @author: yuand
+ * @create: 2019-12-26 15:40
+ **/
+public class AuthErrorController {
+
+    @RequestMapping(value = "/autherror")
+    public Result autherror(int code){
+        return code == 1 ? new Result(ResultCode.UNAUTHENTICATED) : new Result(ResultCode.UNAUTHORISE);
+    }
+}

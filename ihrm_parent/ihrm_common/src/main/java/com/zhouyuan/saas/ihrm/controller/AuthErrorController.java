95f4356067513f9272f50fb3b2c047e5c2d85d37
yuandong <yuand@drplant.com.cn>
2019-12-26 17:13:40 +0800

    shiro测试：公共错误跳转controller的controller注解修改

--- ihrm_parent/ihrm_common/src/main/java/com/zhouyuan/saas/ihrm/controller/AuthErrorController.java
+++ ihrm_parent/ihrm_common/src/main/java/com/zhouyuan/saas/ihrm/controller/AuthErrorController.java
@@ -2,17 +2,22 @@ package com.zhouyuan.saas.ihrm.controller;
 
 import com.zhouyuan.saas.ihrm.entity.Result;
 import com.zhouyuan.saas.ihrm.entity.ResultCode;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @description: shiro认证或鉴权失败controller
  * @author: yuand
  * @create: 2019-12-26 15:40
  **/
-@Controller
+@RestController
 public class AuthErrorController {
 
+    /**
+     * 公共错误跳转
+     * @param code
+     * @return
+     */
     @RequestMapping(value = "/autherror")
     public Result autherror(int code){
         return code == 1 ? new Result(ResultCode.UNAUTHENTICATED) : new Result(ResultCode.UNAUTHORISE);

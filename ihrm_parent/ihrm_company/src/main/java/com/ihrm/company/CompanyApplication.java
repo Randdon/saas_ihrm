ccb1c7f04cb83a09258b86926c8d59a98ccc4aaa
yuandong <yuand@drplant.com.cn>
2019-12-27 16:09:04 +0800

    将企业微服务和系统微服务注册到eureka组件服务端上

--- ihrm_parent/ihrm_company/src/main/java/com/ihrm/company/CompanyApplication.java
+++ ihrm_parent/ihrm_company/src/main/java/com/ihrm/company/CompanyApplication.java
@@ -5,12 +5,14 @@ import com.zhouyuan.saas.ihrm.utils.JwtUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 //Springboot的包扫描
 @SpringBootApplication(scanBasePackages = "com")
 //jpa注解的扫描
 @EntityScan(value = "com.ihrm.domain.company")
+@EnableEurekaClient//开启eureka客户端配置
 public class CompanyApplication {
 
     public static void main(String[] args) {
--- ihrm_parent/ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
+++ ihrm_parent/ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
@@ -5,6 +5,7 @@ import com.zhouyuan.saas.ihrm.utils.JwtUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
@@ -12,6 +13,7 @@ import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 @SpringBootApplication(scanBasePackages = "com")
 //jpa注解的扫描
 @EntityScan(value = "com.ihrm.domain.system")
+@EnableEurekaClient//开启eureka客户端配置
 public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class,args);

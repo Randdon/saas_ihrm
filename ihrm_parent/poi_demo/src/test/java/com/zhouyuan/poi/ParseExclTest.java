5472557e2ad3b511dffa547173bbd0a0e9925353
yuandong <yuand@drplant.com.cn>
2020-01-10 15:59:25 +0800

    使用事件模型解析百万级数据报表

--- ihrm_parent/poi_demo/src/test/java/com/zhouyuan/poi/ParseExclTest.java
+++ ihrm_parent/poi_demo/src/test/java/com/zhouyuan/poi/ParseExclTest.java
@@ -1,13 +1,27 @@
 package com.zhouyuan.poi;
 
+import com.zhouyuan.poi.handler.SheetHandler;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.openxml4j.opc.PackageAccess;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
+import org.apache.poi.xssf.model.SharedStringsTable;
+import org.apache.poi.xssf.model.StylesTable;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -91,5 +105,46 @@ public class ParseExclTest {
                 break;
         }
         return value;
+    }
+
+    /**
+     * 使用事件模式完成百万数据报表的解析
+     * @throws OpenXML4JException
+     * @throws IOException
+     * @throws SAXException
+     */
+    @Test
+    public void parseBillionExcel() throws OpenXML4JException, IOException, SAXException {
+        String path = System.getProperty("user.dir") + File.separator + "test" + File.separator + "billionData.xlsx";
+        //以压缩包的方式打开xlsx文件，设置为只读模式
+        OPCPackage opcPackage = OPCPackage.open(path, PackageAccess.READ);
+        //用压缩包构造XSSFReader对象
+        XSSFReader xssfReader = new XSSFReader(opcPackage);
+
+        //使用工厂创建XMLReader对象
+        XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+
+        //从XSSFReader中获取SharedStringsTable对象
+        SharedStringsTable sharedStringsTable = xssfReader.getSharedStringsTable();
+        //从XSSFReader中获取StylesTable对象
+        StylesTable stylesTable = xssfReader.getStylesTable();
+        //使用StylesTable对象和SharedStringsTable对象以及自定义的事件处理器SheetHandler来构造XSSFSheetXMLHandler事件处理器
+        XSSFSheetXMLHandler xmlHandler =
+                new XSSFSheetXMLHandler(stylesTable,sharedStringsTable,new SheetHandler(),false);
+
+        //将XSSFSheetXMLHandler事件处理器注册到XMLReader对象中
+        xmlReader.setContentHandler(xmlHandler);
+
+        //从XSSFReader中获取该xlsx文件的全部sheet表单集合的迭代器
+        XSSFReader.SheetIterator sheetsData = (XSSFReader.SheetIterator) xssfReader.getSheetsData();
+
+        //循环sheet表单集合，解析每个表单
+        InputSource inputSource;
+        while (sheetsData.hasNext()){
+            inputSource = new InputSource(sheetsData.next());
+            //使用xmlReader解析表单，实际上调用的是注册在xmlReader中的事件处理器来处理，
+            // 即会走我们自定义事件处理器代码中的逻辑，见SheetHandler
+            xmlReader.parse(inputSource);
+        }
     }
 }

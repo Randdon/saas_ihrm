2dfbb5f5c09337dbadec62ef27655d730110c864
yuandong <yuand@drplant.com.cn>
2020-01-13 14:55:21 +0800

    在个人信息返回实体中加入头像字段以解决头像图片页面回显为空的问题

--- ihrm_parent/ihrm_common_model/src/main/java/com/ihrm/domain/system/response/UserResult.java
+++ ihrm_parent/ihrm_common_model/src/main/java/com/ihrm/domain/system/response/UserResult.java
@@ -2,6 +2,8 @@ package com.ihrm.domain.system.response;
 
 import com.ihrm.domain.system.Role;
 import com.ihrm.domain.system.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.BeanUtils;
 
 import java.io.Serializable;
@@ -14,6 +16,8 @@ import java.util.stream.Collectors;
  * @author: yuand
  * @create: 2019-12-16 10:34
  **/
+@Data
+@NoArgsConstructor
 public class UserResult implements Serializable {
 
     private static final long serialVersionUID = 4297464181093070302L;
@@ -95,149 +99,7 @@ public class UserResult implements Serializable {
      */
     private List<String> roleIds;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getEnableState() {
-        return enableState;
-    }
-
-    public void setEnableState(Integer enableState) {
-        this.enableState = enableState;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getCompanyId() {
-        return companyId;
-    }
-
-    public void setCompanyId(String companyId) {
-        this.companyId = companyId;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(String departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public Date getTimeOfEntry() {
-        return timeOfEntry;
-    }
-
-    public void setTimeOfEntry(Date timeOfEntry) {
-        this.timeOfEntry = timeOfEntry;
-    }
-
-    public Integer getFormOfEmployment() {
-        return formOfEmployment;
-    }
-
-    public void setFormOfEmployment(Integer formOfEmployment) {
-        this.formOfEmployment = formOfEmployment;
-    }
-
-    public String getWorkNumber() {
-        return workNumber;
-    }
-
-    public void setWorkNumber(String workNumber) {
-        this.workNumber = workNumber;
-    }
-
-    public String getFormOfManagement() {
-        return formOfManagement;
-    }
-
-    public void setFormOfManagement(String formOfManagement) {
-        this.formOfManagement = formOfManagement;
-    }
-
-    public String getWorkingCity() {
-        return workingCity;
-    }
-
-    public void setWorkingCity(String workingCity) {
-        this.workingCity = workingCity;
-    }
-
-    public Date getCorrectionTime() {
-        return correctionTime;
-    }
-
-    public void setCorrectionTime(Date correctionTime) {
-        this.correctionTime = correctionTime;
-    }
-
-    public Integer getInServiceStatus() {
-        return inServiceStatus;
-    }
-
-    public void setInServiceStatus(Integer inServiceStatus) {
-        this.inServiceStatus = inServiceStatus;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public List<String> getRoleIds() {
-        return roleIds;
-    }
-
-    public void setRoleIds(List<String> roleIds) {
-        this.roleIds = roleIds;
-    }
+    private String staffPhoto;//用户头像
 
     public UserResult(User user) {
         BeanUtils.copyProperties(user,this);

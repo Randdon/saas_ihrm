8bc8d02b215d752403a0c7361713957d108f5d09
yuandong <yuand@drplant.com.cn>
2020-01-15 14:12:44 +0800

    断点续传

--- ihrm_parent/qiniu_demo/src/test/java/com/zhouyuan/qiniu/QiniuDemoTest.java
+++ ihrm_parent/qiniu_demo/src/test/java/com/zhouyuan/qiniu/QiniuDemoTest.java
@@ -7,8 +7,12 @@ import com.qiniu.storage.Configuration;
 import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.storage.persistent.FileRecorder;
 import com.qiniu.util.Auth;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 /**
  * @description: 七牛云测试类
@@ -61,6 +65,47 @@ public class QiniuDemoTest {
             } catch (QiniuException ex2) {
                 //ignore
             }
+        }
+    }
+
+    @Test
+    public void continueLoad(){
+        //构造一个带指定 Region 对象的配置类
+        Configuration cfg = new Configuration(Region.region0());
+        //...其他参数参考类注释
+        //...生成上传凭证，然后准备上传
+        String accessKey = "7VtKGQ4VlMTkZALCoL_nupzOYb3HYZPGEOP_vngt";
+        String secretKey = "FtSvmDWnNpx7TlFp4zMHhPm_lfSOOoUWyuJUXdcz";
+        String bucket = "ihrm-zhou";
+        //如果是Windows情况下，格式是 D:\\qiniu\\test.png
+        String localFilePath = "D:\\projects\\zhouyuan\\saas_ihrm\\ihrm_parent\\qiniu_demo\\test\\test.xlsx";
+        //默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = "testExcel";
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
+        String localTempDir = Paths.get(System.getProperty("java.io.tmpdir"), bucket).toString();
+        System.out.println(localTempDir);
+        try {
+            //设置断点续传文件进度保存目录
+            FileRecorder fileRecorder = new FileRecorder(localTempDir);
+            UploadManager uploadManager = new UploadManager(cfg, fileRecorder);
+            try {
+                Response response = uploadManager.put(localFilePath, key, upToken);
+                //解析上传成功的结果
+                DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+                System.out.println(putRet.key);
+                System.out.println(putRet.hash);
+            } catch (QiniuException ex) {
+                Response r = ex.response;
+                System.err.println(r.toString());
+                try {
+                    System.err.println(r.bodyString());
+                } catch (QiniuException ex2) {
+                    //ignore
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
 
 

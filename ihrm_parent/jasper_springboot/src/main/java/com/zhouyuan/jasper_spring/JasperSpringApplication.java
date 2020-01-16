8d0b7e01522e2e74764a8f8a942f8fc430ba6941
yuandong <yuand@drplant.com.cn>
2020-01-16 11:20:30 +0800

    Springboot整合jasper工程搭建

---
+++ ihrm_parent/jasper_springboot/src/main/java/com/zhouyuan/jasper_spring/JasperSpringApplication.java
@@ -0,0 +1,13 @@
+package com.zhouyuan.jasper_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasperSpringApplication.class, args);
+    }
+
+}
---
+++ ihrm_parent/jasper_springboot/src/test/java/com/zhouyuan/jasper_spring/JasperSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouyuan.jasper_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

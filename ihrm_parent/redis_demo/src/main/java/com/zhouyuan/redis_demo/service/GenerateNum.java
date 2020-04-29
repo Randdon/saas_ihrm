d5586ceb856fb601806077f1ccb467beb85f8d0d
yuandong <yuand@drplant.com.cn>
2020-04-29 17:07:53 +0800

    提交测试

--- ihrm_parent/redis_demo/src/main/java/com/zhouyuan/redis_demo/service/GenerateNum.java
+++ ihrm_parent/redis_demo/src/main/java/com/zhouyuan/redis_demo/service/GenerateNum.java
@@ -1,5 +1,9 @@
 package com.zhouyuan.redis_demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -8,13 +12,21 @@ import java.time.format.DateTimeFormatter;
  * @create: 2020-04-25 09:26
  * @modifiedBy: yuand
  **/
+@Service
 public class GenerateNum {
 
     public static int count = 0;
 
     public int getNum(){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        //在接口并发测试中不加该语句也会出现并发错误
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return count++;
+    }
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    public long redisIncr(){
+        return stringRedisTemplate.opsForValue().increment("redis");
     }
 }
